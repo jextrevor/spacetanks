428d54a282b2b8d74710aae411fb4ef45d1eb67c
Trevor Jex <jextrevor@gmail.com>
2015-08-26 20:19:56 -0600

    Added stuff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100000000 );
 camera.rotation.order = "YZX";
 var group = new THREE.Group();
 group.add(camera);
